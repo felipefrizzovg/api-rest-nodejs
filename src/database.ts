ab264c6e5d947d2ceabbf3e3ad9276dc77fb487d
Felipe Frizzo <felipefrizzovg@gmail.com>
2024-09-29 19:06:00 -0300

    Realizando queries com Knex

--- src/database.ts
+++ src/database.ts
@@ -1,15 +1,15 @@
-import { Knex, knex as setupKnex } from "knex";
+import { Knex, knex as setupKnex } from 'knex'
 
 export const config: Knex.Config = {
-  client: "sqlite",
+  client: 'sqlite',
   connection: {
-    filename: "./db/app.db",
+    filename: './db/app.db',
   },
   useNullAsDefault: true,
   migrations: {
-    extension: "ts",
-    directory: "./db/migrations",
+    extension: 'ts',
+    directory: './db/migrations',
   },
-};
+}
 
-export const knex = setupKnex(config);
+export const knex = setupKnex(config)
